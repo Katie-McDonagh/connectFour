bab91eaf85a0e983c2ef2db8b7efb29f1c0bd4b7
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-02 15:59:01 +0000

    add ability for program to listen out for a mouse click

--- app.js
+++ app.js
@@ -24,15 +24,15 @@ document.addEventListener('DOMContentLoaded', () => {
                   currentPlayer = 1
                   displayCurrentPlayer.innerHTML = currentPlayer
               }
-              //if the sqaure below your current square is not taken, you can't go there
+              //if the sqaure below your current swqaure is not taken, you can't go there
               } else alert('cant go here')
           }
   })(i)
 
-  //check the board for a win or lose 
-  function checkboard(){
-    //const showing the winning arrays
-    const winningArrays = [
+  //check the board for a win or lose
+  function checkBoard() {
+      //make const that shows all winning arrays
+      const winningArrays = [
       [0, 1, 2, 3], [41, 40, 39, 38], [7, 8, 9, 10], [34, 33, 32, 31], [14, 15, 16, 17], [27, 26, 25, 24], [21, 22, 23, 24],
       [20, 19, 18, 17], [28, 29, 30, 31], [13, 12, 11, 10], [35, 36, 37, 38], [6, 5, 4, 3], [0, 7, 14, 21], [41, 34, 27, 20],
       [1, 8, 15, 22], [40, 33, 26, 19], [2, 9, 16, 23], [39, 32, 25, 18], [3, 10, 17, 24], [38, 31, 24, 17], [4, 11, 18, 25],
@@ -44,30 +44,34 @@ document.addEventListener('DOMContentLoaded', () => {
       [22, 23, 24, 25], [26, 25, 24, 23], [29, 30, 31, 32], [33, 32, 31, 30], [36, 37, 38, 39], [40, 39, 38, 37], [7, 14, 21, 28],
       [8, 15, 22, 29], [9, 16, 23, 30], [10, 17, 24, 31], [11, 18, 25, 32], [12, 19, 26, 33], [13, 20, 27, 34]
       ];
-       //now take the 4 values in each winningArray & plug them into the squares values 
-       for(let y = 0; y < winningArrays.length; y++) {
+      //now take the 4 values in earch winningArray & plug them into the squares values 
+      for(let y = 0; y < winningArrays.length; y++) {
           const square1 = squares[winningArrays[y][0]];
           const square2 = squares[winningArrays[y][1]];
           const square3 = squares[winningArrays[y][2]];
           const square4 = squares[winningArrays[y][3]];
 
-        {  //now check those arrays to see if they all have the class of player-one
-            if(square1.classList.contains('player-one') &&
-            square2.classList.contains('player-one') &&
-            square3.classList.contains('player-one') &&
-            square4.classList.contains('player-one')) {
-             //if they do, player-one is passed as the winner
-              result.innerHTML = 'Player one wins!'
-            //remove ability to change result
-            } 
-            //now check to see if they all have the classname player two
-            else if (square1.classList.contains('player-two') &&
-            square2.classList.contains('player-two') &&
-            square3.classList.contains('player-two') &&
-            square4.classList.contains('player-two')) {
-            //if they do, player-two is passed as the winner as well as the chip positions
-            result.innerHTML = 'Player two wins!'
-            }
-        }
-        }}
+          //now check those arrays to see if they all have the class of player-one
+          if(square1.classList.contains('player-one') &&
+              square2.classList.contains('player-one') &&
+              square3.classList.contains('player-one') &&
+              square4.classList.contains('player-one')) {
+                  //if they do, player-one is passed as the winner
+                  result.innerHTML = 'Player one wins!'
+                  //remove ability to change result
+          } 
+          //now check to see if they all have the classname player two
+          else if (square1.classList.contains('player-two') &&
+              square2.classList.contains('player-two') &&
+              square3.classList.contains('player-two') &&
+              square4.classList.contains('player-two')) {
+                  //if they do, player-two is passed as the winner as well as the chip positions
+                  result.innerHTML = 'Player two wins!'
+          }
+      }
   }
+
+  //add an event listener to each square that will trigger the checkBoard function on click
+  squares.forEach(square => square.addEventListener('click', checkBoard))
+
+})
