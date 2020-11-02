76872b28f6a206aa3bb72dbebff1c3b3a6eedfbf
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-02 15:47:03 +0000

    tidy up linting

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const squares = document.querySelectorAll('.grid.div')
+  const squares = document.querySelectorAll('.grid div')
   const result = document.querySelector('#result')
   const displayCurrentPlayer = document.querySelector('#current-player')
   let currentPlayer = 1
 
-  for(var i = 0, len = squares.length; i < len; i++)
+  for (var i = 0, len = squares.length; i < len; i++)
 
   (function(index){
-    //add an onclick for each square of your grid
-    squares[i].onclick = function(){
-       //if the square below your current square has been taken, you can go on top of it)
-       if(squares[index + 7].classList.contains('taken')){
-         if(currentPlayer === 1){
-           squares[index].classList.add('taken')
-           squares[index].classList.add('player-one')
-           //and then change the current player
-           currentPlayer = 2
-           displayCurrentPlayer.innerHTML = currentPlayer
-         } else if (currentPlayer === 2){
-           squares[index].classList.add('taken')
-           squares[index].classList.add('player-two')
-           currentPlayer = 1
-           displayCurrentPlayer.innerHTML = currentPlayer
-         }
-         //if the square below your current square is not taken, you cannot go here
-       } else {
-         alert('cant go there')
-       }
-    }
-  })
-
-})
+  //add an onclick to each square in your grid
+      squares[i].onclick = function(){
+          //if the square below your current square is taken, you can go ontop of it
+          if(squares[index + 7].classList.contains('taken')){
+              if (currentPlayer === 1) {
+                  squares[index].classList.add('taken')
+                  squares[index].classList.add('player-one')
+                  //change the player
+                  currentPlayer = 2
+                  displayCurrentPlayer.innerHTML = currentPlayer
+              } else if (currentPlayer === 2) {
+                  squares[index].classList.add('taken')
+                  squares[index].classList.add('player-two')
+                  //change the player
+                  currentPlayer = 1
+                  displayCurrentPlayer.innerHTML = currentPlayer
+              }
+              //if the sqaure below your current swqaure is not taken, you can't go there
+              } else alert('cant go here')
+          }
+  })(i)
